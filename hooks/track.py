1bb81ca599839892a7245cd90380952ee764c00b
Stefan Hoelzl <1478183+stefanhoelzl@users.noreply.github.com>
2024-10-19 19:37:55 +0200

    [features] tracks overview

--- hooks/track.py
+++ hooks/track.py
@@ -8,11 +8,14 @@ from mkdocs.structure.pages import Page
 from mkdocs.structure.files import Files, File, InclusionLevel
 
 TrackPath = Path(__file__).parent.parent / "tracks"
-TrackPattern = r"track\((?P<name>[^)]+)\)"
-TrackTemplate = f"""
-<div class="track-map">
-  <iframe src="https://gpx.studio/embed?options=TRACK_OPTIONS"></iframe>
-</div>
+TrackPattern = r"track\((?P<name>[^,]+),(?P<start>[^,]+),(?P<destination>[^,]+),(?P<distance>[^,]+),(?P<trip>[^,]+),(?P<total>[^,]+),(?P<ascent>[^,]+),(?P<descent>[^,]+)(,(?P<open>open))?\)"
+TrackTemplate = """
+<details {open}>
+  <summary>{start} - {destination} {distance}km {trip}h / {total}h Ø{speed}km/h ↗{ascent}hm ↘{descent}hm</summary>
+  <div class="track-map">
+    <iframe src="https://gpx.studio/embed?options={options}" loading="lazy"></iframe>
+  </div>
+</details>
 """
 
 OPTIONS = {
@@ -42,12 +45,37 @@ def on_page_markdown(markdown: str, page: Page, config: Config, files: Files) ->
         url = urllib.parse.urljoin(config["site_url"], f"tracks/{match.group('name')}")
         markdown = markdown.replace(
             match.group(),
-            TrackTemplate.replace(
-                "TRACK_OPTIONS",
-                urllib.parse.quote(
+            TrackTemplate.format(
+                start=_rfill(match.group("start"), 15, space="&nbsp;"),
+                destination=_rfill(match.group("destination"), 18, space="&nbsp;"),
+                distance=_lfill(match.group("distance"), 4, space="&nbsp;"),
+                trip=_lfill(_minutes_to_hours(int(match.group("trip"))), 6, space="&nbsp;"),
+                total=_lfill(_minutes_to_hours(int(match.group("total"))), 6, space="&nbsp;"),
+                speed=_lfill(_speed(int(match.group("trip")), float(match.group("distance"))), 4, space="&npbsp;"),
+                ascent=_lfill(match.group("ascent"), 5, space="&nbsp;"),
+                descent=_lfill(match.group("descent"), 6, space="&nbsp;"),
+                open=match.group("open"),
+                options=urllib.parse.quote(
                     json.dumps(
                         {**OPTIONS, "files": [url]})
                 )
             )
         )
     return markdown
+
+
+def _rfill(value: str, size: int, *, space: str = " ") -> str:
+    return f"{value}{space*max(0, size - len(value))}"
+
+
+def _lfill(value: str, size: int, *, space: str = " ") -> str:
+    return f"{space*max(0, size - len(value))}{value}"
+
+def _minutes_to_hours(minutes: int) -> str:
+    hours = minutes / 60
+    full_hours = int(hours)
+    minutes_rest = (minutes - (full_hours * 60))
+    return f"{full_hours}:{minutes_rest:02}"
+
+def _speed(minutes: int, km: float) -> str:
+    return f"{km / (minutes / 60):.1f}"
